29fe1bdbd3cc49bd5b2e0f5bbe2bfb24a126b8c2
kevin2-cyber <keduful@st.ug.edu.gh>
2024-07-05 16:24:10 +0000

    init commit for api

---
+++ nomad-backend/src/test/java/com/kimikevin/nomad_backend/NomadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimikevin.nomad_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ nomad/app/src/main/java/com/mukeju/nomad/model/CourseFee.java
@@ -0,0 +1,8 @@
+package com.mukeju.nomad.model;
+
+public class CourseFee {
+    private Integer courseFeeNo;
+    private String feeDescription;
+    private String fee;
+    private Integer courseNo;
+}
