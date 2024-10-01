7b5e8b95b3ea272c9464b727d3e3ba7f1188fb98
kevin2-cyber <keduful@st.ug.edu.gh>
2024-10-01 22:58:47 +0000

    change

--- nomad-backend/src/main/java/com/kimikevin/nomad_backend/client/ClientConfig.java
+++ nomad-backend/src/main/java/com/kimikevin/nomad_backend/client/ClientConfig.java
@@ -4,18 +4,30 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class ClientConfig {
 
     @Bean
     CommandLineRunner commandLineRunner(ClientRepository clientRepository) {
         return args -> {
-            Client oClient = new Client(
-                    "Kwame",
-                    "Koforidua",
+            Client abena = new Client(
+                    "Abena",
+                    "Sunyani",
                     "0344888000999"
             );
-            clientRepository.save(oClient);
+            Client mariam = new Client(
+                    "Mariam",
+                    "Kumasi",
+                    "0344888000999"
+            );
+            Client kwaku = new Client(
+                    "Kwaku",
+                    "Tamale",
+                    "0344888000999"
+            );
+            clientRepository.saveAll(List.of(abena, mariam, kwaku));
         };
     }
 }
